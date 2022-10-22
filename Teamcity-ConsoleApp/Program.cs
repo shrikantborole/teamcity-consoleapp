120049355baf550bec5b6f89141a3ffde4cca367
shrikantborole <shrikant.borole@gmail.com>
2022-10-22 22:24:58 +0530

    Done with update

--- Teamcity-ConsoleApp/Program.cs
+++ Teamcity-ConsoleApp/Program.cs
@@ -1,2 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!- Shrikant Borole");
+Console.WriteLine("Hello, World!- Shrikant Borole - Some Minute Chnage");
